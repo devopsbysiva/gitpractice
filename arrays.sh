f3462cc7c4cb608b5616e754f8ddab06f3c82bac
gani <foraiprompt73@gmail.com>
2024-09-08 12:14:11 +0530

    added a array list and print statements

---
+++ arrays.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+FRUITS=("apple" "kiwi" "orange" "mango")
+
+echo " The first Fruit is ${FRUITS[0]}"
+echo " The second Fruit is ${FRUITS[1]}"
+echo " The thrid Fruit is ${FRUITS[2]}"
+echo " The fourth Fruit is ${FRUITS[3]}"
