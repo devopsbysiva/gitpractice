f6fc89d82d15d9dd1dd34f5ad1a8b19dfe350e72
gani <foraiprompt73@gmail.com>
2024-09-08 21:17:28 +0530

    correction made in condition file

--- conditions.sh
+++ conditions.sh
@@ -2,6 +2,16 @@
 USERID=$(id -u)
 echo "the userid is :$USERID "
 
+validating(){
+
+    if [ $1 -ne 0 ]
+then
+    echo "there is no package with name git"
+else
+    echo "their is already a package no need to install again"
+fi
+}
+
 if [ $USERID -ne 0 ]
 then
     echo "please connect to root user and execute"
@@ -10,9 +20,4 @@ fi
 
 dnf list installed gitu
 
-if [ $? -ne 0 ]
-then
-    echo "there is no package with name git"
-else
-    echo "their is already a package no need to install again"
-fi
+validating $?
