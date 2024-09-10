9daedef0c77594d53db65b9d66eeb00fa8f07828
gani <foraiprompt73@gmail.com>
2024-09-10 23:17:15 +0530

    added a new package

--- packages_installation.sh
+++ packages_installation.sh
@@ -1,31 +1,31 @@
 #!/bin/bash
+LOG_FOLDER="/var/log/shell_script"
+SCRIPT_NAME= $(echo $0 |cut -d "." -f1)
+DATE=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE="/$LOG_FOLDER/$SCRIPT_NAME-$DATE.log"
+mkdir -p $LOG_FOLDER
 
-# check user is root or not? --done
-#if the user is root then proceed further else stop execution and send alert as please run with sudo privilges. --> done
-#then check for the package if already exist or not.  --> done
-#if already exist then show as this is already installed no need to install.
-#if dosen't exist then install the package and show message as successfully installed.
 R="\e[31m"
 N="\e[0m"
 
 syntax(){
-    echo -e "$R promt :: $0 packagename"
+    echo -e "$R promt :: $0 packagename"  &>>$LOG_FILE
     exit 1
 }
 
 VALIDATE(){
 if [ $1 -ne 0 ]
 then 
-    echo "$2 is FAILED"
+    echo "$2 is FAILED" &>>$LOG_FILE
 else
-    echo "$2 is SUCCESS"
+    echo "$2 is SUCCESS" &>>$LOG_FILE
 fi
 }
 
 USERID=$(id -u) # THE USER ID FOR ROOT IS ALWAYS '0'
 if [ $USERID -ne 0 ]
 then
-    echo "please Execute the script with sudo privileges"
+    echo "please Execute the script with sudo privileges" &>>$LOG_FILE
     exit 1
 fi
 
@@ -39,11 +39,11 @@ do
     dnf list installed $PACKAGE_NAME
     if [ $? -ne 0 ]
     then 
-        echo " $PACKAGE_NAME is preparing to install "
+        echo " $PACKAGE_NAME is preparing to install " &>>$LOG_FILE
         dnf install $PACKAGE_NAME -y
         VALIDATE $? $PACKAGE_NAME
     else
-        echo " $PACKAGE_NAME is already available"
+        echo " $PACKAGE_NAME is already available" &>>$LOG_FILE
     fi
 done
     
