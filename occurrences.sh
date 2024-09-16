155b39788f499f84d6d3bfc44e790ea78ff6b172
gani <foraiprompt73@gmail.com>
2024-09-16 17:08:56 +0530

    added a file deleting_logs

---
+++ occurrences.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+#Write a script that reads a text file and counts the occurrences of each word, 
+#display the top 5 most frequent words along with their counts.
+
+#provide the input of filename.
+FILES=$1
+
+USAGE (){
+    echo "USAGE : : sh occurance.sh <argument>"
+}
+
+if [ $# -eq 0 ]
+then
+    USAGE
+fi
+
+if [ ! -f $FILES ]
+then
+    echo "no such file is found : $FILES"
+fi
+
+#tr -c '[:alnum:]' '[\n*]': Converts non-alphanumeric characters to newlines, effectively isolating words.
+#tr '[:upper:]' '[:lower:]': Converts all words to lowercase for case-insensitive counting.
+#sort: Sorts the words.
+#uniq -c: Counts occurrences of each word.
+#sort -nr: Sorts the words based on frequency in descending order.
+#head -n 5: Displays the top 5 most frequent words
+
+FILE_READ=$(cat namefile.txt | tr -c '[:alnum:]' '[\n*]' | tr '[:upper:]' '[:lower:]' | sort | uniq -c | sort -nr | head -n 5| awk '{print $2 ": " $1}')
+
+
+
+
