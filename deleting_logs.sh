b33b3488e7ec4e6d24e60bf5b08ffdf8764151d9
gani <foraiprompt73@gmail.com>
2024-09-16 14:47:12 +0530

    addedd changes

--- deleting_logs.sh
+++ deleting_logs.sh
@@ -5,7 +5,7 @@
 #2.find the logs greater then 14 days .
 #3. remove the logs.
 
-LOG_FOLDER=/var/log/shell_logs
+LOG_FOLDER="/var/log/shell_logs"
 RETENTION_TIME=14
 
 LOGS=$(ls -l $LOG_FOLDER|find . -name "*.log" -mtime +$RETENTION_TIME)
