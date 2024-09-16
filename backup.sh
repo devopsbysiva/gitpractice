1ae6d8e536ea2e2ae539ffa04a9ebd7242d28017
gani <foraiprompt73@gmail.com>
2024-09-16 16:00:16 +0530

    added a file deleting_logs

--- backup.sh
+++ backup.sh
@@ -13,33 +13,34 @@ DES_DIR=$2
 NO_OF_DAYS={$3:-14}
 TIMESTAMP=$(date +%y-%m-%d-%H-%M-%S)
 
-if [ ! -d $SOU_DIR ]
+if [ ! -d $SOU_DIR ]  # -d checks for directory, ! -false
 then
     echo "there is no such directory: $SOU_DIR"
 fi
 
-if [ ! -d DES_DIR ]
+if [ ! -d DES_DIR ]  # -d checks for directory, ! -false
 then    
     echo "there is no such directory: $DES_DIR"
 fi
 
+#zip [file_name.zip] [file_name] (syntax)
 
 ZIP_FILE=$DES_DIR/app_log-$TIMESTAMP.zip
 
 FILES=$(find $SOU_DIR -name "*.log" -mtime $NO_OF_DAYS)
 
-if [ ! -z $FILES ]
+if [ ! -z $FILES ]  # -z checks for zero files in a directory.
 then
   find ${SOURCE_DIR} -name "*.log" -mtime +14 | zip "$ZIP_FILE" -@
 
-  if [ -f $ZIP_FILE ] 
+  if [ -f $ZIP_FILE ] # -f finds for a  file ina directory.
   then
     echo "files are zipped successfully"
     while IFS= read -r file
     do
         echo "deleting file: $file"
         rm -rf $file
-    done <<< $FILES
+    done <<< $FILES  #providing the input to while condtion from outout of $files
   else
     echo "zipping is failed"
     exit 1 
